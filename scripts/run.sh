ba72a470c20d54b307341e928087aae9d32cc0f1
Jaroslav Knotek <knotekjaroslav@email.cz>
2023-04-21 15:05:57 +0200

    add package setup

--- scripts/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-python python/app.py --imgfolder 'data/20230328/not_labeled' --modelpath 'model/model-prec-20230328.h5' --outputfolder 'output/distributions/20230328'
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MicroPrec',
+    version='0.1',
+    description='Prec -> distribution',
+    author='Jaroslav Knotek',
+    author_email='knotekjaroslav@email.cz',
+    url='https://github.com/jaroslavknotek/micro-precipitates',
+    packages=['precipitates'],
+    python_requires='>=3.9',
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'tqdm',
+        'imageio',
+        'scikit-image',
+        'scikit-learn',
+        'tensorflow',
+        'matplotlib',
+        'pandas'
+    ]
+)
